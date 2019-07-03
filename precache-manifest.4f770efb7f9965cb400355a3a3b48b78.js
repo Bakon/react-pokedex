115a0c877eecd2e477c4e1a9091897979d00973c
Julio Schilders <julio.schilders@gmail.com>
2019-07-03 10:12:11 +0200

    Updates

---
+++ precache-manifest.4f770efb7f9965cb400355a3a3b48b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1af40cebcf2b2c495c6dba5d19b484",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "2273bf9931c2a5c19445",
+    "url": "/react-pokedex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-pokedex/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2273bf9931c2a5c19445",
+    "url": "/react-pokedex/static/js/main.3e7a124b.chunk.js"
+  },
+  {
+    "revision": "49325ecd73a50f0ca547",
+    "url": "/react-pokedex/static/js/runtime~main.0848e284.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
